111300a8e2456d0fbf3ad2eb4bf5ead909253cf2
Chien Tran <trungchien0909@gmail.com>
2025-12-15 16:41:13 +0700

    fix: grid-cols-[1fr_2fr] in MyAppointment page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,7 +9,7 @@ const MyAppointments = () => {
             <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">My Appointments</p>
             <div>
                 {doctors.slice(0, 3).map((item, index) => (
-                    <div key={index} className="grid gird-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b">
+                    <div key={index} className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b">
                         <div>
                             <img className="w-32 bg-indigo-50" src={item.image} alt="" />
                         </div>
