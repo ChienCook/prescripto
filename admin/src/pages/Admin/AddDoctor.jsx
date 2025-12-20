ee84d5906bd3fb8826931f2e26b57bf86d0e091c
Chien Tran <trungchien0909@gmail.com>
2025-12-20 15:03:48 +0700

    feat: loading css while submit form

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -18,6 +18,8 @@ const AddDoctor = () => {
     const [address1, setAddress1] = useState('');
     const [address2, setAddress2] = useState('');
 
+    const [isLoading, setIsLoading] = useState(false);
+
     const { backendUrl, aToken } = useContext(AdminContext);
 
     const onSubmitHandle = async (event) => {
@@ -27,6 +29,8 @@ const AddDoctor = () => {
             if (!docImg) {
                 return toast.error('Image Not Selected');
             }
+
+            setIsLoading(true);
 
             const formData = new FormData();
             // only can submit String type in FormData
@@ -70,6 +74,8 @@ const AddDoctor = () => {
         } catch (error) {
             console.log(error);
             toast.error(error.message);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -232,8 +238,41 @@ const AddDoctor = () => {
                         required
                     />
                 </div>
-                <button type="submit" className="bg-primary text-white px-10 py-3 mt-4 rounded-full">
-                    Add doctor
+                <button
+                    type="submit"
+                    disabled={isLoading}
+                    className={`bg-primary text-white px-10 py-3 mt-4 rounded-full ${
+                        isLoading ? 'opacity-70 cursor-not-allowed' : ''
+                    }`}
+                >
+                    {isLoading ? (
+                        <div className="flex gap-2">
+                            {/* SVG icon xoay tròn */}
+                            <svg
+                                className="animate-spin h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                            </svg>
+                            Adding...
+                        </div>
+                    ) : (
+                        'Add Doctor'
+                    )}
                 </button>
             </div>
         </form>
