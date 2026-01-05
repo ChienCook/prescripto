02bedd6e6658a1ffb0adc5868adbad49d53d45c5
Chien Tran <trungchien0909@gmail.com>
2026-01-05 21:40:40 +0700

    refactor: + display the appointment quantity + add more ConfirmPopup  add-doctor

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -37,7 +37,7 @@ export const AllAppointment = () => {
 
     return (
         <div className="w-full max-w-6xl m-5">
-            <p className="mb-3 text-lg font-medium">All Appointments</p>
+            <p className="mb-3 text-lg font-medium">All Appointments ({appointments.length})</p>
             <div className="bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll">
                 <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
                     <p>#</p>
@@ -81,6 +81,7 @@ export const AllAppointment = () => {
                     </div>
                 ))}
             </div>
+            <ConfirmPopup></ConfirmPopup>
         </div>
     );
 };
